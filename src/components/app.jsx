8de46b490739f6c5a76b2be233928ab1cbf735e6
ashleyliangg <lin.ting.liang.25@dartmouth.edu>
2023-05-17 15:55:34 -0400

    basic extension skeleton

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 function App(props) {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <div className="two-panel">
+        <div className="left-panel">
+          <h2>
+            col 1
+          </h2>
+        </div>
+        <div className="right-panel">
+          <h2>
+            col 2
+          </h2>
+        </div>
+      </div>
+    </div>
   );
 }
 
