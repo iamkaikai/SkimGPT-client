1b5e0920577e76c15276a69c4e0b732cf7722a61
diluo1999 <di.luo.th@dartmouth.edu>
2023-05-25 13:06:55 -0400

    basic adjustable splitview

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SplitPane from 'split-pane-react';
+import 'split-pane-react/esm/themes/default.css';
 import Tools from './tools';
 
 function App(props) {
+  const [sizes, setSizes] = useState(['70%', 'auto']);
+
   return (
-    <div>
+    <div style={{ height: 500 }}>
       <h1>Hello World</h1>
-      <div className="two-panel">
+      <SplitPane
+        split="vertical"
+        sizes={sizes}
+        onChange={setSizes}
+      >
         <div className="left-panel">
-          <h2>
-            col 1
-          </h2>
+          <h2>col 1</h2>
         </div>
         <div className="right-panel">
           <Tools />
         </div>
-      </div>
+      </SplitPane>
     </div>
   );
 }
