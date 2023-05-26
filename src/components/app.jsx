af07561f925013c996028db91a18ae934325c346
diluo1999 <di.luo.th@dartmouth.edu>
2023-05-26 12:39:06 -0400

    enhance split view

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import SplitPane from 'split-pane-react';
+import SplitPane, { Pane } from 'split-pane-react';
 import 'split-pane-react/esm/themes/default.css';
 import Tools from './tools';
 
@@ -7,16 +7,18 @@ function App(props) {
   const [sizes, setSizes] = useState(['70%', 'auto']);
 
   return (
-    <div style={{ height: 500 }}>
-      <h1>Hello World</h1>
+    <div className="app-container">
+      <h1>SkimGPT</h1>
       <SplitPane
         split="vertical"
         sizes={sizes}
         onChange={setSizes}
       >
-        <div className="left-panel">
-          <h2>col 1</h2>
-        </div>
+        <Pane minSize="20%" maxSize="80%">
+          <div className="left-panel">
+            <h2>col 1</h2>
+          </div>
+        </Pane>
         <div className="right-panel">
           <Tools />
         </div>
