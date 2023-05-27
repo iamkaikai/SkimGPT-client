729d1cc36813e8f45bdd38433b0c247ce331fa29
YENKAI HUANG <iamkaikai@livemail.tw>
2023-05-27 17:53:17 -0400

    rewrite for chrom extension

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import 'split-pane-react/esm/themes/default.css';
+// import Tools from './tools';
+
+function sidebar(props) {
+  return (
+    <h2 className="this">sidebar content</h2>
+  );
+}
+
+export default sidebar;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    filename: 'content.js',
+    path: path.resolve(__dirname, 'extension'),
+  },
+};
