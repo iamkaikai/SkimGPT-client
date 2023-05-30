a3852142140cf3a09f45024a5d939f9bcbfcfe5d
YENKAI HUANG <iamkaikai@livemail.tw>
2023-05-30 17:03:03 -0400

    re-upload the reader component

---
+++ src/components/reader.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Toggle from 'react-toggle';
+
+export default function reader(props) {
+  const [mode, setMode] = useState(false);
+
+  const modeChange = (event) => {
+    setMode(event.target.checked);
+  };
+
+  return (
+    <div className="reader-container">
+      <div className="reader-nav">
+        <span className="toggle-des">Reader Mode</span>
+        <Toggle
+          id="cheese-status"
+          defaultChecked={mode}
+          onChange={modeChange}
+        />
+      </div>
+    </div>
+  );
+}
