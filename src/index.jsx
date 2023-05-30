3daf57683bc1939081f3853f641190b61b0c35ba
YENKAI HUANG <iamkaikai@livemail.tw>
2023-05-30 17:00:11 -0400

    separate the reader view to a component

--- src/index.jsx
+++ src/index.jsx
@@ -1,73 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './style.scss';
-import Toggle from 'react-toggle';
-import 'react-toggle/style.css';
 import { createRoot } from 'react-dom/client';
 import SideBar from './components/sidebar';
-
-let done = false;
+import Reader from './components/reader';
 
 // Get a reference to the body element
-const body = document.getElementsByTagName('body')[0];
-
 // Create a new container div
+const body = document.getElementsByTagName('body')[0];
 const mainContainer = document.createElement('div');
 mainContainer.className = 'main-container';
 mainContainer.style.width = '75vw';
-
-// Create a new rvtoggle div for reader view
-const rvtoggle = document.createElement('div');
-rvtoggle.className = 'rvtoggle';
-rvtoggle.style.width = '75vw';
-rvtoggle.style.height = '50px';
-rvtoggle.style.backgroundColor = 'white';
-rvtoggle.style.display = 'flex';
-rvtoggle.style.justifyContent = 'flex-end';
-rvtoggle.style.alignItems = 'center';
-rvtoggle.style.boxShadow = '0px 3px 10px 1px #dfdfdf'; // Add shadow to the bottom
-
-// Create a new div to contain the description text
-const descriptionContainer = document.createElement('div');
-descriptionContainer.textContent = 'Reader View';
-descriptionContainer.style.marginRight = '10px';
-
-// Append the description container to the rvtoggle
-rvtoggle.appendChild(descriptionContainer);
-
-// Create a new div to contain the toggle
-const toggleContainer = document.createElement('div');
-toggleContainer.style.marginRight = '75px';
-
-// Append the toggle container to the rvtoggle
-rvtoggle.appendChild(toggleContainer);
-
-// Render the Toggle component into the container
-ReactDOM.render(
-  <Toggle
-    className="toggle"
-    id="toggle"
-    icons={false}
-  />,
-  toggleContainer,
-);
-
-// Append the rvtoggle to the mainContainer
-mainContainer.appendChild(rvtoggle);
 
 // Move all the existing content into the container
 while (body.firstChild) {
   mainContainer.appendChild(body.firstChild);
 }
 
-// Append the container to the body
+const sidebar = document.createElement('div');
+const reader = document.createElement('div');
+sidebar.className = 'sidebar';
+reader.className = 'reader';
+
+document.body.appendChild(sidebar);
+document.body.appendChild(reader);
+
+const comp1 = createRoot(reader);
+const comp2 = createRoot(sidebar);
+
+comp1.render(<Reader />);
+comp2.render(<SideBar />);
+
 body.appendChild(mainContainer);
-
-done = true;
-
-if (done) {
-  const sidebar = document.createElement('div');
-  document.body.appendChild(sidebar);
-  const root = createRoot(sidebar);
-  root.render(<SideBar />);
-}
