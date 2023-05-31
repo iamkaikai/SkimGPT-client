68b7552b1730185c255ce6b1d9a663396b8e3b52
diluo1999 <di.luo.th@dartmouth.edu>
2023-05-30 22:51:46 -0400

    add reader view wrapper and fade-out animation

--- src/components/reader.jsx
+++ src/components/reader.jsx
@@ -3,12 +3,17 @@ import Toggle from 'react-toggle';
 
 export default function reader(props) {
   const [mode, setMode] = useState(false);
+  const [modeTransition, setModeTransition] = useState(false);
   const modeChange = (event) => {
-    setMode(event.target.checked);
+    setModeTransition(true);
+    setTimeout(() => {
+      setMode(event.target.checked);
+      setModeTransition(false);
+    }, 300);
   };
 
   return (
-    <div className="reader-container">
+    <div className={`reader-container ${modeTransition ? 'mode-transition' : ''}`}>
       <div className="reader-nav">
         <span className="toggle-des">Reader Mode</span>
         <Toggle
@@ -18,6 +23,11 @@ export default function reader(props) {
         />
       </div>
       <div className={`reader-content ${mode ? 'hidden' : ''}`} />
+      {mode && (
+        <div className={`content-wrapper ${mode ? 'show' : ''}`}>
+          <div className="content">CS10</div>
+        </div>
+      )}
     </div>
   );
 }
