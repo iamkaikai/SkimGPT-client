2a7e1854c42574b6bbd00028f397ffdb8b56d279
Annie Tang <annie.tang.25@dartmouth.edu>
2023-05-31 16:49:54 -0400

    added markdown

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,5 +1,6 @@
 // import React, { useState, useEffect } from 'react';
 import React from 'react';
+import { Remarkable } from 'remarkable';
 
 function section(props) {
   console.log(props.section);
@@ -7,11 +8,15 @@ function section(props) {
   const { overview } = props.section;
   const { content } = props.section;
 
+  const md = new Remarkable();
+
+  console.log('in section.jsx');
+
   return (
     <div id="section-container">
-      <p>{title}</p>
-      <p>{overview}</p>
-      <p>{content}</p>
+      <p dangerouslySetInnerHTML={{ __html: md.render(title) }} />
+      <p dangerouslySetInnerHTML={{ __html: md.render(overview) }} />
+      <p dangerouslySetInnerHTML={{ __html: md.render(content) }} />
     </div>
   );
 }
